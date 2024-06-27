c21216786cafd2f76e5a30fbfbf71c4ff7ad3925
James Fu <jf@jfmac.local>
2024-06-27 15:41:20 -0700

    added functions for card_pile

--- scripts/resources/card_pile.gd
+++ scripts/resources/card_pile.gd
@@ -2,3 +2,28 @@ extends Resource
 class_name Card_Pile
 
 @export var cards : Array[Card];
+
+signal card_pile_size_changed
+
+func empty() -> bool:
+	return cards.is_empty()
+
+
+func draw_card() -> Card:
+	var card = cards.pop_front()
+	card_pile_size_changed.emit(cards.size())
+	return card
+
+
+func add_card(card: Card) -> void:
+	cards.append(card)
+	card_pile_size_changed.emit(cards.size())
+
+
+func shuffle() -> void:
+	cards.shuffle()
+
+
+func clear() -> void:
+	cards.clear()
+	card_pile_size_changed.emit(cards.size())
